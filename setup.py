018793806466b98617446c790f6e46dcb3b68496
kiven <kiven.mr@gmail.com>
2017-01-20 10:37:23 +0800

    Add requirements

--- setup.py
+++ setup.py
@@ -2,6 +2,13 @@
 
 from setuptools import setup, find_packages
 
+data = []
+
+install_requires = []
+with open('requirements.txt') as f:
+    for r in f:
+        install_requires.append(r)
+
 setup(
     name="takayi",
     version='0.0.1',
@@ -10,6 +17,7 @@ setup(
     author_email="kiven.mr@gmail.com",
     url="https://github.com/MrKiven/type-doctor",
     packages=find_packages(),
+    package_data={"": ["LICENSE"], "veskit": data},
     tests_require=[
         'pytest==2.5.2',
         'pytest-cov==1.8.1',
