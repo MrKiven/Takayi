5cc888595876211c10e76a3b44e2f8b763b21838
kiven <kiven.mr@gmail.com>
2017-01-23 13:53:25 +0800

    Bump version 0.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="takayi",
-    version="0.1.2",
+    version="0.2.0",
     description="Type hints for python 2.x",
     long_description=open('README.md').read(),
     author="Kiven",
